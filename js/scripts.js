c7b98fdf067f3ed69410115e71037e099fcb8f3e
Joon Yeong Kim <joonyeengyo@gmail.com>
2018-08-03 14:15:44 -0400

    bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,26 @@
-<script>
-    $(document).ready(function() {
-        $("#mycarousel").carousel({ interval: 2000});
-        $("#carouselButton").click(function() {
-            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-                $("#mycarousel").carousel('pause');
-                $("#carouselButton").children("span").removeClass('fa-pause');
-                $("#carouselButton").children("span").addClass('fa-play');
-            } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
-                $("#mycarousel").carousel('cycle');
-                $("#carouselButton").children("span").removeClass('fa-play');
-                $("#carouselButton").children("span").addClass('fa-pause');
-            }
-        })
+$(document).ready(function() {
+    $("#mycarousel").carousel({ interval: 2000});
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
     })
+})
 
-    $(document).ready(function() {
-        $("#reserveModal").modal("hide");
-        $("#reserveButton").click(function() {
-            $("#reserveModal").modal("show");
-        })
+$(document).ready(function() {
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("toggle");
     })
+})
 
-    $(document).ready(function() {
-        $("#loginModal").modal("hide");
-        $("#loginButton").click(function() {
-            $("#loginModal").modal("show");
-        })
+$(document).ready(function() {
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("toggle");
     })
-</script>
+})
